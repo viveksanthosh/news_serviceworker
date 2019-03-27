2acf22c62af567e7c4cdcbf6a8fcf59644280ef8
Viveksanthosh <viveksanthosh@holidayiq.com>
2019-03-27 14:55:17 +0530

    completed the news flow

--- main.js
+++ main.js
@@ -2,23 +2,38 @@ const apiKey = 'ee6e8f12642745248590ff138c7b1134',
     defaultSource = 'bbc-news';
 
 const OptionsDom = document.getElementById('sources');
+const BodyDom = document.getElementById('body');
 
 const getSources = () => fetch(`https://newsapi.org/v2/sources?language=en&apiKey=${apiKey}`)
     .then(r => r.json())
     .then(({ sources }) => sources);
 
-const getNews = (source = defaultSource) => fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`)
+const getNews = source => fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`)
     .then(r => r.json())
     .then(({ articles }) => articles);
 
 
 const main = async () => {
     let sources = await getSources();
+    refreshNews();
     OptionsDom.innerHTML = sources.map(convertSourceToOption).join('\n');
     OptionsDom.value = defaultSource;
+    OptionsDom.onchange = e => {
+        const source = OptionsDom.value;
+        refreshNews(source);
+    }
 };
 
+const refreshNews = async (source = defaultSource) => {
+    let news = await getNews(source);
+    BodyDom.innerHTML = news.map(convertNewsToSection).join('\n');
+}
+
 const convertSourceToOption = s => `<option value='${s.id}'>${s.name}</option>`;
+const convertNewsToSection = n => `<section>
+    <h1>${n.title}</h1>
+    <img src="${n.urlToImage}" style="width: 100%;">
+</section>`;
 
 
 main();
