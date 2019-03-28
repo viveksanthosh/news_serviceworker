8eb12788b020a9dd769cbac78e0b2226e29e9990
Viveksanthosh <viveksanthosh@holidayiq.com>
2019-03-28 16:35:53 +0530

    added a cache first stategy

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', () => {
-    console.log('service worker installed')
+const staticAssets = [
+    './style.css',
+    './main.js'
+];
+self.addEventListener('install', async () => {
+    console.log('service worker installed');
+    const cache = await caches.open('static');
+    cache.addAll(staticAssets);
 });
 
 self.addEventListener('fetch', event => {
-    console.log('fetch')
+    let req = event.request;
+    event.respondWith(cacheFirst(req));
 });
+
+const cacheFirst = async req => {
+    const cachedResponse = await caches.match(req);
+    console.log(cachedResponse)
+    return cachedResponse || fetch(req);
+}
